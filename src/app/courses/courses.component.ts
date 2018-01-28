8b6f4f00a9c982a42555078c26277849c47cc133
Rawa Jalal <rawa.jalal@gmail.com>
2018-01-28 16:19:11 -0500

    event binding

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -4,21 +4,12 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'courses', // <courses>
     template: `
-    <h2>{{ title }}</h2>
-    <img [src]="imageUrl"/>
-    <ul>
-        <li *ngFor="let course of courses">
-            {{ course }}
-        </li>
-    </ul>
+        <button (click) = "onSave($event)">Save</button>
     `
 })
 export class CoursesComponent {
-    title:String = "List of courses";
-    courses:String[];
-    imageUrl:String = "http://lorempixel.com/400/200";
-
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
+    onSave($event){
+        $event.stopPropagation();
+        console.log($event);
     }
 }
